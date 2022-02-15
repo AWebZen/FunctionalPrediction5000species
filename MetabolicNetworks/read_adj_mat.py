2ff7c4fd4092a8b565da9d3fa2f4b557d9954bf0
AWebZen <30926788+AWebZen@users.noreply.github.com>
2022-02-15 18:23:57 +0100

    Update read_adj_mat.py

--- MetabolicNetworks/read_adj_mat.py
+++ MetabolicNetworks/read_adj_mat.py
@@ -18,7 +18,7 @@ node_mapping = {i:n for i,n in enumerate(all_nodes)}
 """
 The adjacency matrices are made through the union of all nodes with all species, which is the list in all_nodes.txt. Therefore, there are actually some nodes that artificially exist in the network, but are isolated. It may be better to remove them.
 """
-m = np.load("adj_mat_species/Enterobacter_hormaechei_subsp._xiangfangensis_eclx.npy", allow_pickle=True).sum() #Random example
+adj_mat = np.load("adj_mat_species/Enterobacter_hormaechei_subsp._xiangfangensis_eclx.npy", allow_pickle=True).sum() #Random example
 G = nx.to_networkx_graph(adj_mat, create_using=nx.DiGraph) #Read adjacency matrix as a directed network
 #Relabel nodes to correct ones
 G = nx.relabel_nodes(G, node_mapping, copy=False)
